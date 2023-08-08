c22b6199e19b13494a57d4f497ed77506725ba33
mux-mux <kmmyemail@ukr.net>
2023-08-08 23:00:18 +0300

    add theme switcher, add js theme switcher

--- js/script.js
+++ js/script.js
@@ -26,3 +26,31 @@ mobileMenuLinks.forEach((item) => {
     toggleMenu();
   });
 });
+
+//Theme switcher
+const themeSwither = document.querySelector('.theme__switch-slider');
+
+const setTheme = (themeName) => {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+};
+
+const toggleTheme = () => {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-light');
+  } else {
+    setTheme('theme-dark');
+  }
+};
+
+(function () {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-dark');
+  } else {
+    setTheme('theme-light');
+  }
+})();
+
+['click', 'touchmove'].forEach((ev) => {
+  themeSwither.addEventListener(ev, toggleTheme);
+});
