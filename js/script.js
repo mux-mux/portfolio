13b4d4d7a37fc694b676cabe95f4cd1cd94d1ffe
mux-mux <kobylyukh@gmail.com>
2024-11-22 14:07:30 +0200

    refactor put js code into functions

--- js/script.js
+++ js/script.js
@@ -1,62 +1,66 @@
 'use strict';
 'use esversion: 8';
 
-const hamburger = document.querySelector('.header__hamburger'),
-  openHamburger = hamburger.querySelector('.header__hamburger-open'),
-  closeHamburger = hamburger.querySelector('.header__hamburger-close'),
-  mobileMenu = document.querySelector('.header__menu'),
-  mobileMenuLink = mobileMenu.querySelectorAll('.header__menu-link');
+function toggleMobileHamburger() {
+  const hamburger = document.querySelector('.header__hamburger'),
+    openHamburger = hamburger.querySelector('.header__hamburger-open'),
+    closeHamburger = hamburger.querySelector('.header__hamburger-close'),
+    mobileMenu = document.querySelector('.header__menu'),
+    mobileMenuLink = mobileMenu.querySelectorAll('.header__menu-link');
 
-const toggleMenu = (action) => {
-  if (action == 'open') {
-    mobileMenu.classList.add('header__menu-active');
-    openHamburger.classList.add('d-none');
-    closeHamburger.classList.remove('d-none');
-  } else {
-    mobileMenu.classList.remove('header__menu-active');
-    openHamburger.classList.remove('d-none');
-    closeHamburger.classList.add('d-none');
-  }
-};
+  const toggleMenu = (action) => {
+    if (action == 'open') {
+      mobileMenu.classList.add('header__menu-active');
+      openHamburger.classList.add('d-none');
+      closeHamburger.classList.remove('d-none');
+    } else {
+      mobileMenu.classList.remove('header__menu-active');
+      openHamburger.classList.remove('d-none');
+      closeHamburger.classList.add('d-none');
+    }
+  };
 
-hamburger.addEventListener('click', () => {
-  mobileMenu.classList.contains('header__menu-active')
-    ? toggleMenu()
-    : toggleMenu('open');
-});
+  hamburger.addEventListener('click', () => {
+    mobileMenu.classList.contains('header__menu-active')
+      ? toggleMenu()
+      : toggleMenu('open');
+  });
 
-mobileMenuLink.forEach((link) => {
-  link.addEventListener('click', toggleMenu);
-});
+  mobileMenuLink.forEach((link) => {
+    link.addEventListener('click', toggleMenu);
+  });
+}
 
-const themeSwither = document.querySelector('.theme__switch-slider'),
-  themeChecker = document.querySelector('.theme__switch-toggle');
+function addThemeSwitcher() {
+  const themeSwither = document.querySelector('.theme__switch-slider'),
+    themeChecker = document.querySelector('.theme__switch-toggle');
 
-const setTheme = (themeName) => {
-  localStorage.setItem('theme', themeName);
-  document.documentElement.className = themeName;
-  themeName === 'theme-dark'
-    ? themeChecker.setAttribute('checked', 'checked')
-    : themeChecker.removeAttribute('checked');
-};
+  const setTheme = (themeName) => {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+    themeName === 'theme-dark'
+      ? themeChecker.setAttribute('checked', 'checked')
+      : themeChecker.removeAttribute('checked');
+  };
 
-const toggleTheme = () => {
-  if (localStorage.getItem('theme') === 'theme-dark') {
-    setTheme('theme-light');
-  } else {
-    setTheme('theme-dark');
-  }
-};
+  const toggleTheme = () => {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+      setTheme('theme-light');
+    } else {
+      setTheme('theme-dark');
+    }
+  };
 
-(function () {
-  if (localStorage.getItem('theme') === 'theme-dark') {
-    setTheme('theme-dark');
-  } else {
-    setTheme('theme-light');
-  }
-})();
+  (function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+      setTheme('theme-dark');
+    } else {
+      setTheme('theme-light');
+    }
+  })();
 
-themeSwither.addEventListener('click', toggleTheme);
+  themeSwither.addEventListener('click', toggleTheme);
+}
 
 function validateInput(e, field, messageContainer) {
   const MINLEN = 2;
@@ -127,25 +131,27 @@ function validateInput(e, field, messageContainer) {
   }
 }
 
-const inputs = document.querySelectorAll('#form input');
+function validateInputsOnInput() {
+  const inputs = document.querySelectorAll('#form input');
 
-inputs.forEach((inputElement) => {
-  const spanToShowMessage = document.createElement('span');
-  spanToShowMessage.classList.add('contact__form-error-message');
-  inputElement.parentNode.insertBefore(
-    spanToShowMessage,
-    inputElement.nextSibling
-  );
+  inputs.forEach((inputElement) => {
+    const spanToShowMessage = document.createElement('span');
+    spanToShowMessage.classList.add('contact__form-error-message');
+    inputElement.parentNode.insertBefore(
+      spanToShowMessage,
+      inputElement.nextSibling
+    );
 
-  inputElement.addEventListener('input', (e) => {
-    validateInput(e, inputElement, spanToShowMessage);
+    inputElement.addEventListener('input', (e) => {
+      validateInput(e, inputElement, spanToShowMessage);
+    });
   });
-});
+}
 
 async function submitForm(event) {
   event.preventDefault();
 
-  function passInputsValidation() {
+  function passedInputsValidation() {
     const messageContainer = document.querySelectorAll(
       '.contact__form-error-message'
     );
@@ -159,46 +165,56 @@ async function submitForm(event) {
 
     return validations.includes(0) ? 0 : 1;
   }
-  if (!passInputsValidation()) return;
+  if (!passedInputsValidation()) return;
 
-  const { button } = event.target.elements;
-  button.setAttribute('disabled', '');
+  async function handleFormSumbit() {
+    const { button } = event.target.elements;
+    button.setAttribute('disabled', '');
 
-  try {
-    const response = await fetch(event.target.action, {
-      method: 'POST',
-      body: new FormData(event.target),
-    });
+    try {
+      const response = await fetch(event.target.action, {
+        method: 'POST',
+        body: new FormData(event.target),
+      });
 
-    if (!response.ok) throw `Server Error: ${response.status}`;
+      if (!response.ok) throw `Server Error: ${response.status}`;
 
-    const contentType = response.headers.get('content-type');
-    if (!contentType || !contentType.includes('application/json')) {
-      throw 'Type error: not JSON';
+      const contentType = response.headers.get('content-type');
+      if (!contentType || !contentType.includes('application/json')) {
+        throw 'Type error: not JSON';
+      }
+
+      const json = await response.json();
+      if (json.result === 'success') {
+        alert(json.info);
+        document
+          .querySelectorAll('#form input, #form textarea')
+          .forEach((inputField) => (inputField.value = ''));
+      } else {
+        console.log(json);
+        throw json.info;
+      }
+    } catch (error) {
+      alert(error);
     }
 
-    const json = await response.json();
-    if (json.result === 'success') {
-      alert(json.info);
-      document
-        .querySelectorAll('#form input, #form textarea')
-        .forEach((inputField) => (inputField.value = ''));
-    } else {
-      console.log(json);
-      throw json.info;
-    }
-  } catch (error) {
-    alert(error);
+    button.removeAttribute('disabled');
   }
-
-  button.removeAttribute('disabled');
+  handleFormSumbit();
 }
 
-console.log(String.raw`
- _     _   _    _______   _     _   ______   ______   ______    
-| |   | | | |  |__   __| | |   | | |  ____| |  __  | |  ____|   
-| |___| | | |     | |    | |___| | | |__    | |__| | | |__      
-|  ___  | | |     | |    |  ___  | |  __|   |  ____| |  __|     
-| |   | | | |     | |    | |   | | | |____  | | \ \  | |____    
-|_|   |_| |_|     |_|    |_|   |_| |______| |_|  \_\ |______|   
+function drawConsoleHi() {
+  console.log(String.raw`
+    _     _   _    _______   _     _   ______   ______   ______    
+    | |   | | | |  |__   __| | |   | | |  ____| |  __  | |  ____|   
+    | |___| | | |     | |    | |___| | | |__    | |__| | | |__      
+    |  ___  | | |     | |    |  ___  | |  __|   |  ____| |  __|     
+    | |   | | | |     | |    | |   | | | |____  | | \ \  | |____    
+    |_|   |_| |_|     |_|    |_|   |_| |______| |_|  \_\ |______|   
                                                                `);
+}
+
+drawConsoleHi();
+validateInputsOnInput();
+toggleMobileHamburger();
+addThemeSwitcher();
